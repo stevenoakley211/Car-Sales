c23e9d161da9721ce1733a0654e86dd5d476eaac
stevenoakley211 <stevenoakley211@live.com>
2020-04-12 21:21:01 -0500

    Fixed styles.scss and removed some variaiables

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {connect ,useDispatch as dispatch} from "react-redux"
+import {connect} from "react-redux"
 import Header from './components/Header';
 import AddedFeatures from './components/AddedFeatures';
 import AdditionalFeatures from './components/AdditionalFeatures';
@@ -21,7 +21,7 @@ const App = (props) => {
     <div className="boxes">
       <div className="box">
         <Header car={props.car} />
-        <AddedFeatures removeItem={removeFeature}car={props.car} />
+        <AddedFeatures removeItem={removeFeature} car={props.car} />
       </div>
       <div className="box">
         <AdditionalFeatures addItem={addFeature} additionalFeatures={props.additionalFeatures} />
